98ba6f27cd98af1ba64f4d8876427ed5ca7d9cab
developerorin2002 <developerorin2002@gmail.com>
2022-11-07 23:23:33 +0600

    basic server setuped

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// server is ready to code 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+// middlewere
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('server is running')
+})
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
