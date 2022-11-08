d401634037d67756ab46276bbd31a45f641fe948
developerorin2002 <developerorin2002@gmail.com>
2022-11-09 01:08:42 +0600

    jwt token start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
@@ -13,7 +14,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async() =>{
-    const serviceCollection = client.db('foodcloud').collection('services')
+    const serviceCollection = client.db('foodcloud').collection('services');
+    const reviewsCollection = client.db('foodcloud').collection('reviews')
     try{
         app.get('/homeservice',async(req,res)=>{
             const query = {};
@@ -34,8 +36,32 @@ const run = async() =>{
             res.send(cursor);
         });
         // review section started
-        app.post('/review',(req,res)=>{
-            console.log(req.body)
+        app.post('/review',async(req,res)=>{
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result); 
+        });
+        app.get('/reviews',async(req,res)=>{
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        // my reviews api
+        app.get('/myreviews',async(req,res)=>{
+            const email = req.query.email;
+            let query = {}
+            if(req.query.email){
+                query = {email:req.query.email}
+            }
+            const cursor = reviewsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            const token = jwt.sign(user,process.env.TOKEN_SECRET,{expiresIn:'5d'});
+            res.send(token);
         })
 
     }
